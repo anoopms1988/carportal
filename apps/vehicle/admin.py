2b8e3737055bc30e908c53705acde9b617970606
anoopms1988 <1988anoopms@gmail.com>
2016-09-07 21:52:01 +0530

    Admin changes

--- apps/vehicle/admin.py
+++ apps/vehicle/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import VehicleType, VehicleFormType, Dealer
+from .models import VehicleType, VehicleFormType, Dealer, Company \
+    , FuelType, RoadAssistance
 
 
 # Register your models here.
@@ -8,14 +9,39 @@ class VehicleTypeAdmin(admin.ModelAdmin):
     model = VehicleType
 
 
+admin.site.register(VehicleType, VehicleTypeAdmin)
+
+
 class VehicleFormTypeAdmin(admin.ModelAdmin):
     model = VehicleFormType
+
+
+admin.site.register(VehicleFormType, VehicleFormTypeAdmin)
 
 
 class DealerAdmin(admin.ModelAdmin):
     model = Dealer
 
 
-admin.site.register(VehicleType, VehicleTypeAdmin)
-admin.site.register(VehicleFormType, VehicleFormTypeAdmin)
 admin.site.register(Dealer, DealerAdmin)
+
+
+class CompanyAdmin(admin.ModelAdmin):
+    model = Company
+
+
+admin.site.register(Company, CompanyAdmin)
+
+
+class FuelTypeAdmin(admin.ModelAdmin):
+    model = FuelType
+
+
+admin.site.register(FuelType, FuelTypeAdmin)
+
+
+class RoadAssistanceAdmin(admin.ModelAdmin):
+    model = RoadAssistance
+
+
+admin.site.register(RoadAssistance, RoadAssistanceAdmin)
