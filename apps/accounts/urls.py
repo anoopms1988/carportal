19519033bc962b4daf6444fab732823074cbb59b
anoopms1988 <1988anoopms@gmail.com>
2016-09-15 22:06:49 +0530

    Confirm code

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -3,4 +3,5 @@ from .views import RegisterView
 
 urlpatterns = [
     url(r'^register/$',RegisterView.as_view(), name='register'),
+    url(r'^confirm/$', RegisterView().confirm_code, name='confirm'),
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -9,6 +9,7 @@ from rest_framework.permissions import AllowAny
 from .models import LoginAttempts, UserProfile, User, EmailConfirmation
 from django.utils.crypto import get_random_string
 from gaadi.http import HttpHandler
+from django.views.decorators.csrf import csrf_exempt
 
 
 @permission_classes((AllowAny,))
@@ -25,6 +26,8 @@ class RegisterView(views.APIView):
             email = data['email']
             username = data['username']
             user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, email=email)
+            user.is_active = False
+            user.save()
             email_confirmation = EmailConfirmation()
             email_confirmation.user = user
             email_confirmation.email_verfied = False
@@ -37,8 +40,43 @@ class RegisterView(views.APIView):
             confirmation_link = "/#/confirm/" + random_token
             to_mail = user.email
             # Email sending logic here
-            return HttpHandler.json_response_wrapper([],
+            return HttpHandler.json_response_wrapper([{'status': True}],
                                                      "Confirmation email is send to the registered email " + to_mail + ". Please check your email",
                                                      status.HTTP_200_OK)
         except Exception as e:
-            return HttpHandler.json_response_wrapper([], e.message, status.HTTP_200_OK)
+            return HttpHandler.json_response_wrapper([{'status': False}], e.message, status.HTTP_200_OK)
+
+
+    @csrf_exempt
+    def confirm_code(self, request):
+        """
+        api to check confirmation code
+        """
+        try:
+            data = request.POST
+            confirmation_code = data.get('confirmation_code', None)
+            try:
+                email_confirmation = EmailConfirmation.objects.filter(created__lte=datetime.datetime.now(),
+                                                                      created__gt=datetime.datetime.now() - datetime.timedelta(
+                                                                          days=3)).filter(
+                    token=confirmation_code).get()
+                selected_user = email_confirmation.user
+            except EmailConfirmation.DoesNotExist:
+                email_confirmation = None
+            if selected_user.is_active:
+                return HttpHandler.json_response_wrapper([{'status': False}], "Already verified", status.HTTP_200_OK,
+                                                         True)
+            if email_confirmation:
+                selected_user.is_active = True
+                selected_user.save()
+                email_confirmation.email_verfied= True
+                email_confirmation.save()
+                return HttpHandler.json_response_wrapper([{'status': True}],
+                                                         "Confirmation successfully verified.Please login to the system",
+                                                         status.HTTP_200_OK, True)
+            else:
+                return HttpHandler.json_response_wrapper([{'status': False}], "Failure", status.HTTP_200_OK,
+                                                         True)
+        except Exception as e:
+            return HttpHandler.json_response_wrapper([{'status': False}], e.message, status.HTTP_200_OK,
+                                                     True)
